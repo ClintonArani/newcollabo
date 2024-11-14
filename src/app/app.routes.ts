2e87f2c065813031f225693a65707340dba9948d
Clinton Arani <clintonarani420@gmail.com>
2024-11-14 09:40:33 +0300

    well designed homepage

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,13 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from './component/home/home.component';
+import { LoginComponent } from './component/login/login.component';
+import { RegisterComponent } from './component/register/register.component';
+import { NotfoundComponent } from './component/notfound/notfound.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {path: 'home',component: HomeComponent},
+    {path: '',redirectTo: 'home',pathMatch: 'full'},
+    {path: 'login', component: LoginComponent},
+    {path: 'register',component: RegisterComponent},
+    {path: '**',component: NotfoundComponent}
+];
